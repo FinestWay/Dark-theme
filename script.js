99999f0b28ca20913e6a6f9d207822c7891bcf73
FinestWay <rrai30430@gmail.com>
2024-02-18 16:15:59 +0530

    changes in styling of webpage

--- script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector("#btn");
-const div = document.querySelector("#div")
-div.innerHTML = 1;
-
-btn.addEventListener("click", () => {
-    div.innerHTML = div.innerHTML++;
-})
